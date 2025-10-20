1a2ff4f71857a634e105c442f974550454f47e01
joss-ham <josshamblen@gmail.com>
2025-10-20 15:41:13 +0100

    initial setup

---
+++ src/main/java/com/example/boids/BoidsApplication.java
@@ -0,0 +1,13 @@
+package com.example.boids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoidsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoidsApplication.class, args);
+    }
+
+}
