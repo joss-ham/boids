15c8789a59c250cf521e26522c9ab2befd753e70
joss-ham <josshamblen@gmail.com>
2025-10-22 18:41:49 +0100

    addwd attributes for boid class

--- src/main/java/com/example/boids/model/Boid.java
+++ src/main/java/com/example/boids/model/Boid.java
@@ -1,4 +1,14 @@
 package com.example.boids.model;
 
 public class Boid {
+    private double x;
+    private double y;
+
+    private double vx;
+    private double vy;
+
+    public Boid(double x, double y, double vx, double vy) {
+        this.x = x;
+        this.y = y;
+    }
 }
