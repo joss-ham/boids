fec134d50344e3f0330a00be5863c915b263290a
joss-ham <josshamblen@gmail.com>
2025-10-29 09:28:44 +0000

    addwd attributes for boid class

--- src/main/java/com/example/boids/model/Boid.java
+++ src/main/java/com/example/boids/model/Boid.java
@@ -1,14 +1,55 @@
 package com.example.boids.model;
 
 public class Boid {
+    //declare attributes
     private double x;
     private double y;
 
     private double vx;
     private double vy;
 
-    public Boid(double x, double y, double vx, double vy) {
+    private double id;
+
+    public Boid(double x, double y, double vx, double vy, double id) {
+        //constructor
         this.x = x;
         this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.id = id;
     }
+    //getters
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+    public double getVx() {
+        return vx;
+    }
+    public double getVy() {
+        return vy;
+    }
+    public double getId() {
+        return id;
+    }
+    //setters
+    public void setX(double x) {
+        this.x = x;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+    public void setVx(double vx) {
+        this.vx = vx;
+
+    }
+    public void setVy(double vy) {
+        this.vy = vy;
+    }
+    public void setId(double id) {
+        this.id = id;
+    }
+
 }
--- src/main/java/com/example/boids/service/SimulationService.java
+++ src/main/java/com/example/boids/service/SimulationService.java
@@ -1,4 +1,11 @@
 package com.example.boids.service;
 
+import com.example.boids.model.Boid;
+
+import java.util.List;
+
 public class SimulationService {
+    private List<Boid> boids;
+    private final double width = 800;
+    private final double height = 600;
 }
